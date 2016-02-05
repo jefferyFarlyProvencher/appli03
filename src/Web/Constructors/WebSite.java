5e1d4d26696f78acc805923d0e8f5075df6ae516
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2016-02-05 12:04:03 -0500

    Test

---
+++ src/Web/Constructors/WebSite.java
@@ -0,0 +1,15 @@
+package Web.Constructors;
+
+import java.util.ArrayList;
+
+public class WebSite {
+	
+	public static String    UrlBase;
+	private static String[] ProTags;
+	private static String[] ConTags;
+
+    public WebSite() { }
+    
+	public String GetPagination(int i) { return ""; }
+	public String GetUrl() { return UrlBase; }
+}
