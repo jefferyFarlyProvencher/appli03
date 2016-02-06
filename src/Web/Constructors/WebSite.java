640f9e4d21320c1fde85b8cf34935dc177d7501a
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2016-02-06 15:24:37 -0500

    Updated WebSite Object

--- src/Web/Constructors/WebSite.java
+++ src/Web/Constructors/WebSite.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 
 public class WebSite {
 	
-	public static String baseURL;
+	public String baseURL;
+	public String name;
+	public String protocol;
+	public String extension;
 
-    public WebSite() { }
-    
-	public String GetPagination(int i) { return ""; }
-	public String GetUrl() { return baseURL; }
+    public WebSite(String name, String protocol, String extension ) {
+		this.name = name;
+		this.protocol = protocol;
+		this.extension = extension;
+		this.buildURL();
+	}
+
+	private void buildURL(){
+		this.baseURL = this.protocol + "://www." + this.baseURL + this.extension;
+	}
+
+	public String GetURL(){
+		return this.baseURL;
+	}
 }
